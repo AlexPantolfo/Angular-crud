447a323c7a38ffd9c6d3050a81b9bcd86d27630e
Alex Alves Pantolfo <alexpantolfo619@gmail.com>
2022-09-25 11:13:26 -0300

    Adding the dialog component

--- src/app/new-product-dialog/new-product-dialog.component.ts
+++ src/app/new-product-dialog/new-product-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-product-dialog',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewProductDialogComponent implements OnInit {
 
+  public freshnesslist: String[] = ['Brand New', 'Second Hand', `Refurbished`]
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log('productform', this.productform)
   }
+
+  productform = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    category: new FormControl(''),
+    dateproduct: new FormControl(''),
+    price: new FormControl(''),
+    comment: new FormControl('')
+  })
 
 }
